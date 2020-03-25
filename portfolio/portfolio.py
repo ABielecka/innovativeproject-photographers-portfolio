e344a46ce8c51fe7c0534667eea60e454008d5fa
AlutkaG <alagerlach@interia.pl>
2020-03-25 14:39:16 +0100

    Added about and photos

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -5,16 +5,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    pics = os.listdir('static/img/')
-    return render_template('index.html', name='Jan Kowalski', pics=pics)
+    pics = os.listdir('static/img/kotoslaw_mrasny/')
+    return render_template('index.html', name='Kotoslaw Mrasny', pics=pics)
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title='About', name='Jan Kowalski')
+    return render_template('about.html', title='About', name='Kotoslaw Mrasny')
 
 @app.route("/contact")
 def contact():
-    return render_template('contact.html', title='Contact', name='Jan Kowalski')
+    return render_template('contact.html', title='Contact', name='Kotoslaw Mrasny')
 
 if __name__ == "__main__":
     app.run(debug==True)
